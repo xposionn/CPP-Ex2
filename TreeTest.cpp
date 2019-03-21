82706e18fc49fe9fc958e6ceed03c1a29cba6d49
Liad Cohen - Ubuntu VM <liad.code@gmail.com>
2019-03-21 17:39:01 +0200

    added new executable test file compiled. given tests is now called erelTests . -m -m a

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -36,40 +36,40 @@ int main() {
   .CHECK_EQUAL (threetree.left(5), 3)
   .CHECK_EQUAL (threetree.right(5), 7)
   .CHECK_THROWS(threetree.insert(3))
-  .CHECK_OK    (threetree.print())
+  //.CHECK_OK    (threetree.print())
 
 //Added tests:
-  .check_equal (increasetree.size(), 0) //new tree should have size zero.
-  .check_ok (increasetree.insert(0)) //inserting integers in increasing order. tree should be as follows:
+  .CHECK_EQUAL (increasetree.size(), 0) //new tree should have size zero.
+  .CHECK_OK (increasetree.insert(0)) //inserting integers in increasing order. tree should be as follows:
   //                                    0
-  .check_ok (increasetree.insert(1)) //   1
-  .check_ok (increasetree.insert(2)) //     2
-  .check_ok (increasetree.insert(3)) //       3
-  .check_equal (increasetree.size(), 4) //tree size should be 4. we inserted 4 elements.
-  .check_throws (increasetree.parent(0)) //there should be no parent for node "0".
-  .check_equal (increasetree.parent(1),0) //parent of element "1" should be the root, a.k.a  "0".
-  .check_throws (increasetree.left(3)) //there are no childs for node "3".
-  .check_throws (increasetree.right(3)) //there are no childs for node "3".
-  .check_throws (increasetree.left(0)) //there are no left-childs for the root "0".
-  .check_equal (increasetree.right(0),1) //there right child of root node "0" should be "1".
-  .check_equal (increasetree.getSize(),4) //function "getSize" should return 4.
-  .check_ok (increasetree.find(2)) //function "find" should pass as "2" is inside the tree.
-  .check_throws (increasetree.find(0)) //function "find" should fail as "0" is NOT inside the tree.
-  .check_ok (increasetree.root()) //function "root" should pass as there is a root for the tree.
+  .CHECK_OK (increasetree.insert(1)) //   1
+  .CHECK_OK (increasetree.insert(2)) //     2
+  .CHECK_OK (increasetree.insert(3)) //       3
+  .CHECK_EQUAL (increasetree.size(), 4) //tree size should be 4. we inserted 4 elements.
+  .CHECK_THROWS (increasetree.parent(0)) //there should be no parent for node "0".
+  .CHECK_EQUAL (increasetree.parent(1),0) //parent of element "1" should be the root, a.k.a  "0".
+  .CHECK_THROWS (increasetree.left(3)) //there are no childs for node "3".
+  .CHECK_THROWS (increasetree.right(3)) //there are no childs for node "3".
+  .CHECK_THROWS (increasetree.left(0)) //there are no left-childs for the root "0".
+  .CHECK_EQUAL (increasetree.right(0),1) //there right child of root node "0" should be "1".
+  .CHECK_EQUAL (increasetree.getSize(),4) //function "getSize" should return 4.
+  .CHECK_OK (increasetree.find(2)) //function "find" should pass as "2" is inside the tree.
+  .CHECK_THROWS (increasetree.find(0)) //function "find" should fail as "0" is NOT inside the tree.
+  .CHECK_OK (increasetree.root()) //function "root" should pass as there is a root for the tree.
 
-  .check_ok (negativetree.insert(-1)) //adding negative integers in degreasing order.
-  .check_ok (negativetree.insert(-2))
-  .check_ok (negativetree.insert(-3))
-  .check_equal (negativetree.size(),3)  //size should be 3
-  .check_equal (negativetree.left(-1),-2)  //left child of ROOT ("-1") node should be "-2"
-  .check_equal (negativetree.left(-2),-3) //left child of INSIDE_NODE("-2") node should be "-3"
-  .check_ok (negativetree.insert(0)) //we now insert a larger-than-every-node "0" to the tree.
-  .check_equal (negativetree.parent(0),-1) //parent of the "0" node, which we inserted last, should be the right chile of -1. thus, parent of 0 is -1.
-  .check_equal (negativetree.right(-1),0) //explained above
-  .check_throws (negativetree.left(0)) //last-inserted node "0" should have no childs!
-  .check_throws (negativetree.right(0)) //last-inserted node "0" should have no childs!
+  .CHECK_OK (negativetree.insert(-1)) //adding negative integers in degreasing order.
+  .CHECK_OK (negativetree.insert(-2))
+  .CHECK_OK (negativetree.insert(-3))
+  .CHECK_EQUAL (negativetree.size(),3)  //size should be 3
+  .CHECK_EQUAL (negativetree.left(-1),-2)  //left child of ROOT ("-1") node should be "-2"
+  .CHECK_EQUAL (negativetree.left(-2),-3) //left child of INSIDE_NODE("-2") node should be "-3"
+  .CHECK_OK (negativetree.insert(0)) //we now insert a larger-than-every-node "0" to the tree.
+  .CHECK_EQUAL (negativetree.parent(0),-1) //parent of the "0" node, which we inserted last, should be the right chile of -1. thus, parent of 0 is -1.
+  .CHECK_EQUAL (negativetree.right(-1),0) //explained above
+  .CHECK_THROWS (negativetree.left(0)) //last-inserted node "0" should have no childs!
+  .CHECK_THROWS (negativetree.right(0)) //last-inserted node "0" should have no childs!
 
-  .check_throws (negativetree.parent(-1))
+  .CHECK_THROWS (negativetree.parent(-1))
 
   .print();
 
